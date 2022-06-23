98efeb6af3674c655fba440935bae60ba13f90f0
Bernardo Barreto <bernardo.barreto@ua.pt>
2022-06-23 16:56:06 +0100

    final assignment 2

--- Assignment2/src/deployAndRun.sh
+++ Assignment2/src/deployAndRun.sh
@@ -1,5 +1,7 @@
 xterm  -T "General Repository" -hold -e "./GeneralReposDeployAndRun.sh" &
+sleep 1
 xterm  -T "Table" -hold -e "./TableDeployAndRun.sh" &
+sleep 1
 xterm  -T "Bar" -hold -e "./BarDeployAndRun.sh" &
 xterm  -T "Kitchen" -hold -e "./KitchenDeployAndRun.sh" &
 sleep 1
