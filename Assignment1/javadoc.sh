43d339d7a929a92df3f009542350cf93ae0abf56
Bernardo Barreto <bernardo.barreto@ua.pt>
2022-07-11 17:37:55 +0100

    assignment1 - javadoc script update done

--- Assignment1/javadoc.sh
+++ Assignment1/javadoc.sh
@@ -1,4 +1,4 @@
-cd /home/bernardo/Documents/Ano4/SD/Praticas/SD/Restaurant/src
-javadoc -cp "/home/bernardo/Documents/Ano4/SD/Praticas/SD/Restaurant/src/genclass.jar" ./*/*.java -d ../javadoc
+cd /home/bernardo/Documents/Ano4/SD/Praticas/SD/Assignment1/src
+javadoc -cp "/home/bernardo/Documents/Ano4/SD/Praticas/SD/Assignment1/src/genclass.jar" ./*/*.java -d ../javadoc
 cd ../javadoc
 google-chrome index.html
