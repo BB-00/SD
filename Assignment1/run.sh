ee0d8a851f90a9c0ee4c31d0093a1708acc85902
Bernardo Barreto <bernardo.barreto@ua.pt>
2022-07-14 23:22:17 +0100

    assignment1 - run para 10 vezes

--- Assignment1/run.sh
+++ Assignment1/run.sh
@@ -1,7 +1,7 @@
 cd /home/bernardo/Documents/Ano4/SD/Praticas/SD/Assignment1/src/
 javac -cp /home/bernardo/Documents/Ano4/SD/Praticas/SD/Assignment1/src/genclass.jar ./*/*.java
 
-for i in $(seq 1 1000)
+for i in $(seq 1 10)
 do
 echo -e "\nRun n.o " $i
 java -cp .:/home/bernardo/Documents/Ano4/SD/Praticas/SD/Assignment1/src/genclass.jar main.Main >> output.txt
